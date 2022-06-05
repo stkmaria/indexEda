431f657c9ecf727bf559b79fa401dd07631850f0
stkmaria <106670782+stkmaria@users.noreply.github.com>
2022-06-05 16:05:37 +0300

    Add files via upload

---
+++ индексЕда/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){ 
+ $('.owl-carousel').owlCarousel({
+     items: 1,
+     loop: true,
+     nav: true,
+     dots: false,
+     autoplay: true,
+     smartSpeed: 500
+ });   
+});
