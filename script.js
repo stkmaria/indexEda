daef9470c489ce06a4439af3fa6e704ddc891b3f
stkmaria <106670782+stkmaria@users.noreply.github.com>
2022-06-10 14:31:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){ 
+ $('.owl-carousel').owlCarousel({
+     items: 1,
+     loop: true,
+     nav: true,
+     dots: false,
+     autoplay: true,
+     smartSpeed: 500
+ });
+ $('#tabset').tabs();   
+});
